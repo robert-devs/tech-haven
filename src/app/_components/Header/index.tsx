de8ff7e78497b0834db903ecacaca671ec0dd19e
Robert Maina <robert-devs>
2023-12-04 15:16:26 +0300

    set-up and adding header

---
+++ src/app/_components/Header/index.tsx
@@ -0,0 +1,22 @@
+{
+  /* eslint-disable @next/next/no-img-element */
+}
+
+import { Header } from '../../../payload/payload-types'
+import { fetchHeader } from '../../_api/fetchGlobals'
+import HeaderComponent from './HeaderComponent'
+
+export async function Header() {
+  let header: Header | null = null
+
+  try {
+    header = await fetchHeader()
+  } catch (error) {}
+  console.log(header)
+
+  return (
+    <>
+      <HeaderComponent header={header} />
+    </>
+  )
+}
